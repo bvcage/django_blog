3ace527b87f6e9a24cf158fa77e8ef945e15fb2a
Bailey Cage <bailey.cage@gmail.com>
2023-06-05 12:16:07 -0400

    setup API for blog posts

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import BlogPostListView, BlogPostDetailView
 
 urlpatterns = [
-    path('api/', include('rest_framework.urls')),
+    # path('api/', include('rest_framework.urls')),
+    path('', BlogPostListView.as_view()),
+    path('<slug>', BlogPostDetailView.as_view())
 ]
